cc8550bcbd52a1ae29009b0a03b7e75bb7e6f99a
William Klausmeyer <willyk95@gmail.com>
2025-03-08 10:52:15 -0700

    Make the weather page more robust

--- public/js/weather.js
+++ public/js/weather.js
@@ -49,9 +49,8 @@ fetch('/weather.json')
 			+ Object.keys(data.stateFires)
 				.filter(key => typeof fireTypeLabels[key] !== 'undefined')
 				.map(key => `${padEndWithSpaces(fireTypeLabels[key], maxFireTypeLabelLen)} ... ${padStartWithSpaces(data.stateFires[key][0], 8)} ... ${padStartWithSpaces(data.stateFires[key][1], 8)}`)
-				.join('<br>')
-			+ `<br><a href="https://gacc.nifc.gov/rmcc/">RM GACC Incident Map</a>`;
+				.join('<br>');
 
-		insertionPoints.alerts.innerHTML = `${data.weather || '<b>No active alerts</b>'}<br><a href="https://forecast.weather.gov/MapClick.php?lon=-105.6988059170544&lat=37.9934785363087#.YmFqWPPMIeY">Crestone 7 Day Forecast</a>`;
-		insertionPoints.restrictions.innerHTML = `${data.bans}<br><a href="https://www.google.com/maps/d/u/0/embed?mid=1cEAhNHqp82AXABF8qU7k6sRFI4392V0e&ll=38.91583034559255%2C-106.1196738784554&z=7">Colorado Restriction Map</a>`;
+		insertionPoints.alerts.innerHTML = data.weather || '<b>No active alerts</b>';
+		insertionPoints.restrictions.innerHTML = data.bans;
 	});
