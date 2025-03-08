c26a473d9374fa30ee04ce8efdd79e0c73edec62
William Klausmeyer <willyk95@gmail.com>
2025-03-08 10:52:15 -0700

    Add a default weather alert message

--- public/js/weather.js
+++ public/js/weather.js
@@ -52,6 +52,6 @@ fetch('/weather.json')
 				.join('<br>')
 			+ `<br><a href="https://gacc.nifc.gov/rmcc/">RM GACC Incident Map</a>`;
 
-		insertionPoints.alerts.innerHTML = `${data.weather}<br><a href="https://forecast.weather.gov/MapClick.php?lon=-105.6988059170544&lat=37.9934785363087#.YmFqWPPMIeY">Crestone 7 Day Forecast</a>`;
+		insertionPoints.alerts.innerHTML = `${data.weather || '<b>No active alerts</b>'}<br><a href="https://forecast.weather.gov/MapClick.php?lon=-105.6988059170544&lat=37.9934785363087#.YmFqWPPMIeY">Crestone 7 Day Forecast</a>`;
 		insertionPoints.restrictions.innerHTML = `${data.bans}<br><a href="https://www.google.com/maps/d/u/0/embed?mid=1cEAhNHqp82AXABF8qU7k6sRFI4392V0e&ll=38.91583034559255%2C-106.1196738784554&z=7">Colorado Restriction Map</a>`;
 	});
