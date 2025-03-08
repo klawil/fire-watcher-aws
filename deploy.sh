690447994796609a5522c2712ad3552c54a1ba32
William Klausmeyer <willyk95@gmail.com>
2025-03-08 10:52:17 -0700

    Move the CDK deploy command in the deployment process

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 source ~/.zshrc
 nvm use 14
 
+cdk deploy
+
 cd website
 jekyll build
 cd ..
-
-cdk deploy
 aws s3 cp --recursive ./website/_site s3://***REMOVED***/ --acl bucket-owner-full-control --metadata-directive REPLACE
