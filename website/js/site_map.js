d1160cdb654529b6dc4dc8b09bb82b4fa4e90fd6
William Klausmeyer <willyk95@gmail.com>
2025-03-08 10:52:19 -0700

    Remove the site map page and put it on the status page

--- website/js/site_map.js
+++
@@ -1,7 +0,0 @@
-const map = L.map('map').setView([ 37.749, -106.073 ], 8);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-updateSitesTable(true);
--- website/js/status.js
+++ website/js/status.js
@@ -51,7 +51,12 @@ const periodFormatters = [
 	},
 ];
 
-updateSitesTable();
+const map = L.map('map').setView([ 37.749, -106.073 ], 8);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+updateSitesTable(true);
 
 const color1 = {
 	backgroundColor: 'rgba(54, 162, 235, 0.5)',
