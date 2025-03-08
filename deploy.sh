3f6ca91453c792c6d6ec28977585bccd0057c305
William Klausmeyer <willyk95@gmail.com>
2025-03-08 10:52:20 -0700

    Remove some files that should not have been deployed to S3

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,9 @@ cdk deploy --all
 npm run build-website
 cd website
 jekyll build
+rm _site/webpack.conf.js
+rm _site/tsconfig.json
+rm -rf _site/ts
+rm _site/js/*.LICENSE.txt
 cd ..
 aws s3 cp --recursive ./website/_site s3://***REMOVED***/ --acl bucket-owner-full-control --metadata-directive REPLACE
