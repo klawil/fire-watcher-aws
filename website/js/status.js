162fc4993a87209d2e004c733f6d917fa402fe45
William Klausmeyer <willyk95@gmail.com>
2025-03-08 10:52:18 -0700

    Add more options to the status page

--- website/js/status.js
+++ website/js/status.js
@@ -64,7 +64,7 @@ const color2 = {
 	borderColor: 'rgb(255, 99, 132)'
 };
 
-const charts = [
+const baseCharts = [
 	{
 		id: 'api-calls',
 		query: 'metrics=api-frontend,api-infra,api-user,api-twilio,s3-created,queue'
@@ -114,173 +114,225 @@ const charts = [
 	},
 ];
 
-charts.forEach(chart => {
-	fetch(`${baseHost}/api/frontend?action=stats&${chart.query}`)
-		.then(r => r.json())
-		.then(data => {
-			chart.val = chart.val || (val => val);
+let currentCharts = {};
 
-			const names = data.data.names;
-			const chartData = {};
-			const labels = [];
+let refreshConfig = null;
+let lastInitId = null;
+function refreshCharts() {
+	const thisInitId = Date.now()
+	lastInitId = thisInitId;
+	Promise.all(charts.map(chart => fetch(`${baseHost}/api/frontend?action=stats&${chart.query}`)
+			.then(r => r.json())
+			.then(data => {
+				if (lastInitId !== thisInitId) return;
 
-			for (let t = data.startTime; t < data.endTime; t += (data.period * 1000)) {
-				const dateStr = new Date(t).toISOString();
-				chartData[dateStr] = {};
-				labels.push(dateStr);
-			}
+				chart.val = chart.val || (val => val);
 
-			data.data.data.forEach(item => {
-				Object.keys(names)
-					.forEach(key => chartData[item.ts][key] = item.values[key] || 0);
-			});
-			labels.forEach(label => Object.keys(names)
-				.forEach(key => chartData[label][key] = chart.val(chartData[label][key]) || 0));
+				const names = data.data.names;
+				const chartData = {};
+				const labels = [];
 
-			const formatter = periodFormatters.reduce((f, val) => {
-				if (data.period <= val.period) return val.formatter;
-
-				return f;
-			}, periodFormatters[periodFormatters.length - 1].formatter);
-
-			const datasets = Object.keys(names)
-				.map(key => ({
-					label: names[key],
-					data: labels.map(label => chartData[label][key]),
-					fill: false,
-					tension: 0.1,
-					pointStyle: false
-				}));
-			const chartConfig = {
-				type: 'line',
-				data: {
-					labels: labels.map(label => formatter(new Date(label))),
-					datasets
-				},
-				options: {
-					interaction: {
-						mode: 'x'
-					}
+				for (let t = data.startTime; t < data.endTime; t += (data.period * 1000)) {
+					const dateStr = new Date(t).toISOString();
+					chartData[dateStr] = {};
+					labels.push(dateStr);
 				}
-			};
-			if (window.innerWidth >= 992) {
-				chartConfig.options.plugins = {
-					legend: {
-						position: 'right'
-					}
-				};
-			} else {
-				chartConfig.options.plugins = {
-					legend: {
-						position: 'bottom'
-					}
-				};
-			}
-			if (chart.yMax) {
-				chartConfig.options.scales = {
-					y: {
-						max: chart.yMax
-					}
-				};
-			}
-			if (chart.stacked) {
-				chartConfig.options.scales = chartConfig.options.scales || {};
-				chartConfig.options.scales.y = chartConfig.options.scales.y || {};
-				chartConfig.options.scales.y.stacked = true;
-			}
-			if (chart.fill && datasets.length === 2) {
-				chartConfig.options.scales = {
-					y: {
-						min: 0,
-						max: 45
-					}
-				};
-				datasets.forEach(dataset => dataset.label = dataset.label.split(' - ').pop());
-				datasets[0].borderColor = color1.borderColor;
-				datasets[0].backgroundColor = color1.backgroundColor;
-				datasets[0].showLine = false;
-				datasets[0].fill = {
-					target: 2,
-					above: color1.backgroundColor,
-					below: 'rgba(0,0,0,0)'
-				};
-				datasets[1].borderColor = color2.borderColor;
-				datasets[1].backgroundColor = color2.backgroundColor;
-				datasets[1].showLine = false;
-				datasets[1].fill = {
-					target: 2,
-					below: color2.backgroundColor,
-					above: 'rgba(0,0,0,0)'
-				};
-				datasets.push(({
-					backgroundColor: 'rgba(0,0,0,0)',
-					fillColor: 'rgba(0,0,0,0)',
-					fill: false,
-					pointStyle: false,
-					tension: 0.0,
-					label: 'none',
-					data: labels.map((label, i) => {
-						const dataA = datasets[0].data[i];
-						const dataB = datasets[1].data[i];
 
-						if (dataA < 30 && dataB < 30) {
-							return dataA;
+				data.data.data.forEach(item => {
+					Object.keys(names)
+						.forEach(key => chartData[item.ts][key] = item.values[key] || 0);
+				});
+				labels.forEach(label => Object.keys(names)
+					.forEach(key => chartData[label][key] = chart.val(chartData[label][key]) || 0));
+
+				const formatter = periodFormatters.reduce((f, val) => {
+					if (data.period <= val.period) return val.formatter;
+
+					return f;
+				}, periodFormatters[periodFormatters.length - 1].formatter);
+
+				const datasets = Object.keys(names)
+					.map(key => ({
+						label: names[key],
+						data: labels.map(label => chartData[label][key]),
+						fill: false,
+						tension: 0.1,
+						pointStyle: false
+					}));
+				const chartConfig = {
+					type: 'line',
+					data: {
+						labels: labels.map(label => formatter(new Date(label))),
+						datasets
+					},
+					options: {
+						interaction: {
+							mode: 'x'
 						}
-						if (dataA > 30 && dataB > 30) {
-							return dataB;
+					}
+				};
+				if (window.innerWidth >= 992) {
+					chartConfig.options.plugins = {
+						legend: {
+							position: 'right'
 						}
-						return 30;
-					})
-				}))
-				chartConfig.options.plugins = {
-					annotation: {
-						annotations: {
-							line1: {
-								type: 'line',
-								label: {
-									content: 'Service Degraded',
-									display: true,
-									color: '#000',
-									backgroundColor: 'transparent',
-									yAdjust: 10
-								},
-								yMin: 30,
-								yMax: 30,
-								borderColor: 'rgb(255, 99, 132)',
-          			borderWidth: 2
-							},
-							line2: {
-								type: 'line',
-								label: {
-									content: 'Optimal Service',
-									display: true,
-									color: '#000',
-									backgroundColor: 'transparent',
-									yAdjust: -10
-								},
-								yMin: 40,
-								yMax: 40,
-								borderColor: color1.borderColor,
-          			borderWidth: 2
+					};
+				} else {
+					chartConfig.options.plugins = {
+						legend: {
+							position: 'bottom'
+						}
+					};
+				}
+				if (chart.yMax) {
+					chartConfig.options.scales = {
+						y: {
+							max: chart.yMax
+						}
+					};
+				}
+				if (chart.stacked) {
+					chartConfig.options.scales = chartConfig.options.scales || {};
+					chartConfig.options.scales.y = chartConfig.options.scales.y || {};
+					chartConfig.options.scales.y.stacked = true;
+				}
+				if (chart.fill && datasets.length === 2) {
+					chartConfig.options.scales = {
+						y: {
+							min: 0,
+							max: 45
+						}
+					};
+					datasets.forEach(dataset => dataset.label = dataset.label.split(' - ').pop());
+					datasets[0].borderColor = color1.borderColor;
+					datasets[0].backgroundColor = color1.backgroundColor;
+					datasets[0].showLine = false;
+					datasets[0].fill = {
+						target: 2,
+						above: color1.backgroundColor,
+						below: 'rgba(0,0,0,0)'
+					};
+					datasets[1].borderColor = color2.borderColor;
+					datasets[1].backgroundColor = color2.backgroundColor;
+					datasets[1].showLine = false;
+					datasets[1].fill = {
+						target: 2,
+						below: color2.backgroundColor,
+						above: 'rgba(0,0,0,0)'
+					};
+					datasets.push(({
+						backgroundColor: 'rgba(0,0,0,0)',
+						fillColor: 'rgba(0,0,0,0)',
+						fill: false,
+						pointStyle: false,
+						tension: 0.0,
+						label: 'none',
+						data: labels.map((label, i) => {
+							const dataA = datasets[0].data[i];
+							const dataB = datasets[1].data[i];
+
+							if (dataA < 30 && dataB < 30) {
+								return dataA;
 							}
+							if (dataA > 30 && dataB > 30) {
+								return dataB;
+							}
+							return 30;
+						})
+					}))
+					chartConfig.options.plugins = {
+						annotation: {
+							annotations: {
+								line1: {
+									type: 'line',
+									label: {
+										content: 'Service Degraded',
+										display: true,
+										color: '#000',
+										backgroundColor: 'transparent',
+										yAdjust: 10
+									},
+									yMin: 30,
+									yMax: 30,
+									borderColor: 'rgb(255, 99, 132)',
+									borderWidth: 2
+								},
+								line2: {
+									type: 'line',
+									label: {
+										content: 'Optimal Service',
+										display: true,
+										color: '#000',
+										backgroundColor: 'transparent',
+										yAdjust: -10
+									},
+									yMin: 40,
+									yMax: 40,
+									borderColor: color1.borderColor,
+									borderWidth: 2
+								}
+							}
+						},
+						legend: {
+							display: false,
+							labels: {
+								filter: a => a.text !== 'none'
+							}
+						},
+						tooltip: {
+							filter: (a, b, c) => a.dataset.label !== 'none' &&
+								c.map(v => v.dataset.label).indexOf(a.dataset.label) === b
 						}
-					},
-					legend: {
-						display: false,
-						labels: {
-							filter: a => a.text !== 'none'
-						}
-					},
-					tooltip: {
-						filter: (a, b, c) => a.dataset.label !== 'none' &&
-							c.map(v => v.dataset.label).indexOf(a.dataset.label) === b
-					}
-				};
-			}
+					};
+				}
 
-			new Chart(
-				document.getElementById(chart.id),
-				chartConfig
-			);
+
+				if (typeof currentCharts[chart.id] === 'undefined')
+					currentCharts[chart.id] = new Chart(
+						document.getElementById(chart.id),
+						chartConfig
+					);
+				else {
+					currentCharts[chart.id].options = chartConfig.options;
+					currentCharts[chart.id].data = chartConfig.data;
+					currentCharts[chart.id].update();
+				}
+			})))
+		.then(() => {
+			if (thisInitId !== lastInitId) return;
+
+			if (refreshConfig !== null)
+				setTimeout(refreshCharts, refreshConfig);
 		});
+}
+
+let charts = baseCharts;
+refreshCharts();
+
+const realtimeData = document.getElementById('realtime-switch');
+const refreshData = document.getElementById('refresh-interval');
+
+realtimeData.addEventListener('change', () => {
+	if (!realtimeData.checked) {
+		charts = baseCharts;
+		refreshCharts();
+		return;
+	}
+
+	charts = baseCharts.map(chart => {
+		const newChart = { ...chart };
+		newChart.query = `${chart.query.split('&')[0]}&period=60&timerange=7200000&live=y`;
+
+		return newChart
+	});
+	refreshCharts();
+});
+
+refreshData.addEventListener('change', () => {
+	if (refreshData.value === 'off') {
+		refreshConfig = null;
+	} else {
+		refreshConfig = parseInt(refreshData.value, 10);
+		refreshCharts();
+	}
 });
