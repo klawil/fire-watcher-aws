97ee62ec461344e4c83b9b1d9dae49d6a1b10a62
William Klausmeyer <willyk95@gmail.com>
2025-03-08 10:52:21 -0700

    Add the ability to send texts from the UI

--- website/ts/texts.ts
+++ website/ts/texts.ts
@@ -1,8 +1,12 @@
-import { ApiFrontendListTextsResponse, TextObject } from '../../common/frontendApi';
+import { ApiFrontendListTextsResponse, AnnouncementApiBody, TextObject } from '../../common/frontendApi';
 import { doneLoading } from './utils/loading';
 import { createTableRow } from './utils/table';
-import { authInit } from './utils/auth';
+import { authInit, user } from './utils/auth';
 import { getLogger } from '../../stack/resources/utils/logger';
+import { pagingConfig, PagingTalkgroup, pagingTalkgroupOrder, UserDepartment, validDepartments } from '../../common/userConstants';
+import { showAlert } from './utils/alerts';
+import { changeButtonColor, modifyButton } from './utils/button';
+import { ApiUserLoginResult } from '../../common/userApi';
 
 const logger = getLogger('texts');
 
@@ -166,7 +170,7 @@ function buildTable(
 async function init() {
 	logger.trace('init', ...arguments);
 
-	Promise.all([
+	await Promise.all([
 		true,
 		false,
 	].map(async page => {
@@ -195,6 +199,102 @@ async function init() {
 			page
 		)
 	}));
+
+	const departmentSelect = document.getElementById('department') as HTMLSelectElement;
+	const talkgroupSelect = document.getElementById('talkgroup') as HTMLSelectElement;
+	const sendButton = document.getElementById('text-send') as HTMLButtonElement;
+	const textBodyInput = document.getElementById('text-body') as HTMLTextAreaElement;
+	const testModeSwitch = document.getElementById('text-test') as HTMLInputElement;
+	const inputs: (HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement)[] = [
+		departmentSelect,
+		talkgroupSelect,
+		textBodyInput,
+		testModeSwitch,
+	];
+	const textBody: AnnouncementApiBody = {
+		body: '',
+	};
+	if (!user.isDistrictAdmin) {
+		talkgroupSelect.hidden = true;
+		(document.getElementById('text-test-container') as HTMLDivElement).hidden = true;
+	} else {
+		pagingTalkgroupOrder
+			.forEach(tg => {
+				const option = document.createElement('option');
+				talkgroupSelect.appendChild(option);
+				option.value = tg.toString();
+				option.innerHTML = pagingConfig[tg].partyBeingPaged;
+			});
+		talkgroupSelect.addEventListener('change', () => {
+			if (talkgroupSelect.value === 'none') {
+				delete textBody.talkgroup;
+			} else {
+				textBody.talkgroup = Number(talkgroupSelect.value) as PagingTalkgroup;
+				delete textBody.department;
+				departmentSelect.value = 'none';
+			}
+		});
+	}
+	validDepartments
+		.filter(dep => user.isDistrictAdmin || (user[dep]?.active && user[dep]?.admin))
+		.forEach((dep, i, a) => {
+			const option = document.createElement('option');
+			departmentSelect.appendChild(option);
+			option.value = dep;
+			option.innerHTML = dep;
+
+			if (a.length === 1) {
+				option.selected = true;
+				departmentSelect.disabled = true;
+				textBody.department = dep;
+			}
+		});
+	departmentSelect.addEventListener('change', () => {
+		if (departmentSelect.value === 'none') {
+			delete textBody.department;
+		} else {
+			textBody.department = departmentSelect.value as UserDepartment;
+			delete textBody.talkgroup;
+			talkgroupSelect.value = 'none';
+		}
+	});
+	sendButton.addEventListener('click', async () => {
+		inputs.forEach(input => input.classList.remove('is-invalid'));
+		textBody.body = textBodyInput.value;
+		textBody.test = testModeSwitch.checked && user.isDistrictAdmin;
+		if (textBody.body === '') {
+			showAlert('danger', 'You must have something in the body to send a text');
+			return;
+		}
+		if (
+			typeof textBody.department === 'undefined' &&
+			typeof textBody.talkgroup === 'undefined'
+		) {
+			departmentSelect.classList.add('is-invalid');
+			talkgroupSelect.classList.add('is-invalid');
+			showAlert('danger', 'You must select a department to send a message to');
+			return;
+		}
+
+		modifyButton(sendButton, 'secondary', 'Sending', true, false);
+		const result = await fetch(`/api/frontend?action=announce`, {
+			method: 'POST',
+			body: JSON.stringify(textBody),
+		}).then(r => r.json()) as ApiUserLoginResult;
+		modifyButton(sendButton, 'primary', 'Send Message', false, true);
+		if (!result.success) {
+			changeButtonColor(sendButton, 'danger');
+			inputs
+				.filter(input => result.errors.includes(input.name))
+				.forEach(input => input.classList.add('is-invalid'));
+		} else {
+			if (!departmentSelect.disabled) {
+				departmentSelect.value = 'none';
+			}
+			talkgroupSelect.value = 'none';
+			textBodyInput.value = '';
+		}
+	});
 
 	doneLoading();
 }
