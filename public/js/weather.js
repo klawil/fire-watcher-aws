83a0665ffabcdc7821b7c4e7e2280b9db6f779b5
William Klausmeyer <willyk95@gmail.com>
2025-03-08 10:52:15 -0700

    Add the weather data to the page (rudimentary)

--- public/js/weather.js
+++ public/js/weather.js
@@ -1,3 +1,9 @@
 fetch('/weather.json')
 	.then(r => r.json())
-	.then(console.log);
+	.then(data => {
+		document.body.innerHTML += data.weather;
+		document.body.innerHTML += `<br><br><b>Fire Restrictions</b><br>`;
+		document.body.innerHTML += data.bans;
+
+		console.log(data);
+	});
