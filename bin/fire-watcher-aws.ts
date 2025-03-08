2dfce81302e46a67cb464967dde3780e68c1935a
William Klausmeyer <willyk95@gmail.com>
2025-03-08 10:52:14 -0700

    Reset to the default CDK app

--- bin/fire-watcher-aws.ts
+++ bin/fire-watcher-aws.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { FirePagerStack } from '../lib/fire-pager-stack';
+import { FireWatcherAwsStack } from '../lib/fire-watcher-aws-stack';
 
 const app = new cdk.App();
-new FirePagerStack(app, 'FirePagerStack', {
+new FireWatcherAwsStack(app, 'FireWatcherAwsStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- lib/fire-watcher-aws-stack.ts
+++ lib/fire-watcher-aws-stack.ts
@@ -2,14 +2,14 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class FirePagerStack extends Stack {
+export class FireWatcherAwsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
 
     // example resource
-    // const queue = new sqs.Queue(this, 'TestQueue', {
+    // const queue = new sqs.Queue(this, 'FireWatcherAwsQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
   }
