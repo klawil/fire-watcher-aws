2298aae316dd01d2180df290e82bc083bb44298c
William Klausmeyer <willyk95@gmail.com>
2025-03-09 20:17:02 -0600

    Add the bottom navbar

--- website-react/src/components/layout.tsx
+++ website-react/src/components/layout.tsx
@@ -3,6 +3,7 @@
 import { useDarkMode } from "@/logic/clientHooks";
 import { PageConfig } from "@/types/page";
 import CofrnNavbar from "./navbar";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 export default function CofrnLayout({
   children,
@@ -25,6 +26,31 @@ export default function CofrnLayout({
 
       {pageConfig.title && <h1 className="text-center">{pageConfig.title}</h1>}
       {children}
+
+      <Navbar
+        bg={modeName}
+        className="mt-4"
+      >
+        <Container
+          fluid={true}
+          className="justify-content-center"
+        >
+          <Navbar.Brand href="#">
+            <img
+              src="/favicon.png"
+              width="30"
+              height="24"
+              className="d-inline-block align-top"
+              alt="COFRN"
+              style={{
+                filter: `invert(${modeName === 'dark' ? 1 : 0})`,
+              }}
+            />
+          </Navbar.Brand>
+          <Nav.Link className="mx-4" href="/about">About Us</Nav.Link>
+          <Navbar.Text className="mx-4">© 2025 First Responder Notifications, LLC</Navbar.Text>
+        </Container>
+      </Navbar>
     </>
   );
 }
