19ecae649547f2d2fced3c026a4751efd877d999
William Klausmeyer <willyk95@gmail.com>
2025-06-02 18:52:00 -0600

    Add the sourcemaps to the production build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  productionBrowserSourceMaps: true,
 };
 
 if (process.env.PROD_BUILD) {
