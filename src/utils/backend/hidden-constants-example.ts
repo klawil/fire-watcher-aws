97ac6b9e19a1020ddbd0cbd251d9e880f5230ce9
William Klausmeyer <willyk95@gmail.com>
2026-03-28 12:26:21 -0600

    Make the github build work

---
+++ src/utils/backend/hidden-constants-example.ts
@@ -0,0 +1,11 @@
+/**
+ * This file contains constants that are used in the backend but should not be committed. Constants
+ * that are used in the CDK build process should be placed in the .env file
+ */
+
+export const TEST_USER_PHONE = '7195807822';
+export const FORWARD_EMAIL_TO = 'willyk95@gmail.com';
+export const BILLING_EMAIL_ADDRESS = 'billing@cofrn.org';
+export const PAY_TO_NAME = 'William Klausmeyer';
+export const PAY_TO_ADDRESS_LINE_1 = 'PO Box 983';
+export const PAY_TO_ADDRESS_LINE_2 = 'Crestone, CO 81131';
