a932b404a937ce193435188cda80b0c611467b68
William Klausmeyer <willyk95@gmail.com>
2026-03-28 12:26:21 -0600

    Make the github build work

---
+++ src/utils/backend/hidden-constants-example.ts
@@ -0,0 +1,11 @@
+/**
+ * This file contains constants that are used in the backend but should not be committed. Constants
+ * that are used in the CDK build process should be placed in the .env file
+ */
+
+export const TEST_USER_PHONE = '5555555555';
+export const FORWARD_EMAIL_TO = 'test@test.com';
+export const BILLING_EMAIL_ADDRESS = 'billing@test.com';
+export const PAY_TO_NAME = 'John Doe';
+export const PAY_TO_ADDRESS_LINE_1 = '123 Main St';
+export const PAY_TO_ADDRESS_LINE_2 = 'Anytown, USA 12345';
